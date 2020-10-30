46f51ecdd9c9b979a7036a8bffe6fce64e19c5a8
Andrew Burkett <burkett.andrew@gmail.com>
2020-10-29 17:00:16 -0700

    Reduce exe name

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,29 @@
-use std::process::{Command,ExitStatus};
-use std::env::{current_exe};
-use std::io;
+use std::env::current_exe;
 use std::ffi::OsString;
+use std::io;
+use std::process::{Command, ExitStatus};
 
 use thiserror::Error;
 
-#[derive(Debug,Error)]
+#[derive(Debug, Error)]
 enum Error {
     #[error("Failed to determine filename of current executable")]
     NoFileName,
     #[error("Failed to determine folder of current executable")]
     NoParent,
     #[error("IO Error: {0}")]
-    IO(#[from] io::Error)
+    IO(#[from] io::Error),
 }
 
-type Result<T> = std::result::Result<T,Error>;
+type Result<T> = std::result::Result<T, Error>;
 
 fn _main() -> Result<ExitStatus> {
     let exe = current_exe()?;
     let exename = exe.file_name().ok_or(Error::NoFileName)?;
     let parent = exe.parent().ok_or(Error::NoParent)?;
     let mut sdv_exename = OsString::new();
-    sdv_exename.push("sdv.");
     sdv_exename.push(exename);
+    sdv_exename.push(".sdv.exe");
     let sdv_exe = parent.join(sdv_exename);
     let args = std::env::args_os();
     let s = Command::new(sdv_exe).args(args).status()?;
@@ -36,7 +36,7 @@ fn main() {
             std::process::exit(s.code().unwrap_or(2));
         }
         Err(e) => {
-            eprintln!("Error: {}",e);
+            eprintln!("Error: {}", e);
             std::process::exit(1);
         }
     }

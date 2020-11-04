d1754604aab74deba53bb8bae5955e82a457966a
Andrew Burkett <burkett.andrew@gmail.com>
2020-11-03 16:31:35 -0800

    Set pre op for read/write/create

--- FsFilter1/FsFilter1.c
+++ FsFilter1/FsFilter1.c
@@ -26,7 +26,7 @@ ULONG_PTR OperationStatusCtx = 1;
 #define PTDBG_TRACE_ROUTINES            0x00000001
 #define PTDBG_TRACE_OPERATION_STATUS    0x00000002
 
-ULONG gTraceFlags = 0;
+ULONG gTraceFlags = PTDBG_TRACE_ROUTINES;
 
 
 #define PT_DBG_PRINT( _dbgLevel, _string )          \
@@ -133,204 +133,18 @@ EXTERN_C_END
 //
 
 CONST FLT_OPERATION_REGISTRATION Callbacks[] = {
-
-#if 0 // TODO - List all of the requests to filter.
-    { IRP_MJ_CREATE,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_CREATE_NAMED_PIPE,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_CLOSE,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
     { IRP_MJ_READ,
       0,
       FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
+      NULL },
     { IRP_MJ_WRITE,
       0,
       FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_QUERY_INFORMATION,
+      NULL },
+    { IRP_MJ_CREATE,
       0,
       FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_SET_INFORMATION,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_QUERY_EA,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_SET_EA,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_FLUSH_BUFFERS,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_QUERY_VOLUME_INFORMATION,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_SET_VOLUME_INFORMATION,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_DIRECTORY_CONTROL,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_FILE_SYSTEM_CONTROL,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_DEVICE_CONTROL,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_INTERNAL_DEVICE_CONTROL,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_SHUTDOWN,
-      0,
-      FsFilter1PreOperationNoPostOperation,
-      NULL },                               //post operations not supported
-
-    { IRP_MJ_LOCK_CONTROL,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_CLEANUP,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_CREATE_MAILSLOT,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_QUERY_SECURITY,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_SET_SECURITY,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_QUERY_QUOTA,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_SET_QUOTA,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_PNP,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_ACQUIRE_FOR_SECTION_SYNCHRONIZATION,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_RELEASE_FOR_SECTION_SYNCHRONIZATION,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_ACQUIRE_FOR_MOD_WRITE,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_RELEASE_FOR_MOD_WRITE,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_ACQUIRE_FOR_CC_FLUSH,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_RELEASE_FOR_CC_FLUSH,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_FAST_IO_CHECK_IF_POSSIBLE,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_NETWORK_QUERY_OPEN,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_MDL_READ,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_MDL_READ_COMPLETE,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_PREPARE_MDL_WRITE,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_MDL_WRITE_COMPLETE,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_VOLUME_MOUNT,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-    { IRP_MJ_VOLUME_DISMOUNT,
-      0,
-      FsFilter1PreOperation,
-      FsFilter1PostOperation },
-
-#endif // TODO
+      NULL },
 
     { IRP_MJ_OPERATION_END }
 };
@@ -654,39 +468,28 @@ Return Value:
 {
     NTSTATUS status;
 
+    // UNREFERENCED_PARAMETER( Data );
     UNREFERENCED_PARAMETER( FltObjects );
     UNREFERENCED_PARAMETER( CompletionContext );
 
     PT_DBG_PRINT( PTDBG_TRACE_ROUTINES,
                   ("FsFilter1!FsFilter1PreOperation: Entered\n") );
 
-    //
-    //  See if this is an operation we would like the operation status
-    //  for.  If so request it.
-    //
-    //  NOTE: most filters do NOT need to do this.  You only need to make
-    //        this call if, for example, you need to know if the oplock was
-    //        actually granted.
-    //
-
-    if (FsFilter1DoRequestOperationStatus( Data )) {
-
-        status = FltRequestOperationStatusCallback( Data,
-                                                    FsFilter1OperationStatusCallback,
-                                                    (PVOID)(++OperationStatusCtx) );
-        if (!NT_SUCCESS(status)) {
-
-            PT_DBG_PRINT( PTDBG_TRACE_OPERATION_STATUS,
-                          ("FsFilter1!FsFilter1PreOperation: FltRequestOperationStatusCallback Failed, status=%08x\n",
-                           status) );
-        }
+    PFLT_FILE_NAME_INFORMATION FileNameInfo;
+    FLT_FILE_NAME_OPTIONS Options = FLT_FILE_NAME_NORMALIZED 
+      // Not Sure about this option
+      // It says it will query if safe not sure the circumstances when its not safe
+      | FLT_FILE_NAME_QUERY_DEFAULT;
+    status = FltGetFileNameInformation( Data, Options, &FileNameInfo );
+    if (NT_SUCCESS(status)) {
+      FltReleaseFileNameInformation(FileNameInfo);
+    } else {
+      PT_DBG_PRINT( PTDBG_TRACE_ROUTINES,
+                    ("FsFilter1!FsFilter1PreOperation: FltGetFileNameInformation Failed, status=%08x\n",
+                      status) );
     }
 
-    // This template code does not do anything with the callbackData, but
-    // rather returns FLT_PREOP_SUCCESS_WITH_CALLBACK.
-    // This passes the request down to the next miniFilter in the chain.
-
-    return FLT_PREOP_SUCCESS_WITH_CALLBACK;
+    return FLT_PREOP_SUCCESS_NO_CALLBACK;
 }
 
 

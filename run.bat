67a80a08471c781ac5a49b374a42328ea9d01e4a
Andrew Burkett <burkett.andrew@gmail.com>
2022-02-05 16:08:19 -0800

    Remove ssh code run.bat. Have it run on local machine

--- run.bat
+++ run.bat
@@ -1,8 +1,6 @@
 @REM disable secure boot if needed
 @REM bcdedit -set testsigning on
-ssh user@WINDEV2010EVAL "net stop fsfilter1"
-copy /y x64\Release\FsFilter1.* \\WINDEV2010EVAL\share
-ssh user@WINDEV2010EVAL "rundll32.exe advpack.dll,LaunchINFSectionEx C:\share\FsFilter1.inf,,,4" || exit /b 1
-ssh user@WINDEV2010EVAL "net start fsfilter1" || exit /b 1
-copy /y target\release\*.exe \\WINDEV2010EVAL\share
-ssh user@WINDEV2010EVAL "C:\share\sdv-user.exe" || exit /b 1
+net stop fsfilter1
+rundll32.exe advpack.dll,LaunchINFSectionEx %~dp0x64\Release\FsFilter1.inf,,,4 || exit /b 1
+net start fsfilter1 || exit /b 1
+target\release\sdv-user.exe || exit /b 1

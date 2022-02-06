b97a7a37612f7da4f74da488a787e396fee0050d
Andrew Burkett <burkett.andrew@gmail.com>
2022-02-05 16:09:45 -0800

    Rename user prog to sdv

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@
 net stop fsfilter1
 rundll32.exe advpack.dll,LaunchINFSectionEx %~dp0x64\Release\FsFilter1.inf,,,4 || exit /b 1
 net start fsfilter1 || exit /b 1
-target\release\sdv-user.exe || exit /b 1
+target\release\sdv.exe || exit /b 1
